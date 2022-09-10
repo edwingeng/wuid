46273b8d17683862f96a4ea293926360d5df0714
gengbing <edwingeng@126.com>
2022-09-10 11:11:02 +0800

    internal: add coverage.sh and vet.sh

---
+++ internal/coverage.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+[[ "$TRACE" ]] && set -x
+pushd `dirname "$0"` > /dev/null
+trap __EXIT EXIT
+
+colorful=false
+tput setaf 7 > /dev/null 2>&1
+if [[ $? -eq 0 ]]; then
+    colorful=true
+fi
+
+function __EXIT() {
+    popd > /dev/null
+}
+
+function printError() {
+    $colorful && tput setaf 1
+    >&2 echo "Error: $@"
+    $colorful && tput setaf 7
+}
+
+function printImportantMessage() {
+    $colorful && tput setaf 3
+    >&2 echo "$@"
+    $colorful && tput setaf 7
+}
+
+function printUsage() {
+    $colorful && tput setaf 3
+    >&2 echo "$@"
+    $colorful && tput setaf 7
+}
+
+go test -cover -coverprofile=c.out -v "$@" && go tool cover -html=c.out
