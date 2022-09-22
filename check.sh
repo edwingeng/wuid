7a3c49e25cf74bbd5a8df06c71306c110eca15a9
gengbing <edwingeng@126.com>
2022-09-22 08:49:49 +0800

    add check.sh

---
+++ check.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+[[ "$TRACE" ]] && set -x
+pushd `dirname "$0"` > /dev/null
+trap __EXIT EXIT
+
+colorful=false
+tput setaf 7 > /dev/null 2>&1
+if [[ $? -eq 0 ]]; then
+    colorful=true
+fi
+
+function __EXIT() {
+    popd > /dev/null
+}
+
+function printError() {
+    $colorful && tput setaf 1
+    >&2 echo "Error: $@"
+    $colorful && tput setaf 7
+}
+
+function printImportantMessage() {
+    $colorful && tput setaf 3
+    >&2 echo "$@"
+    $colorful && tput setaf 7
+}
+
+function printUsage() {
+    $colorful && tput setaf 3
+    >&2 echo "$@"
+    $colorful && tput setaf 7
+}
+
+find . -name coverage.sh | xargs -n 1 bash -c \
+    'cmp --silent internal/coverage.sh "$0" || echo "files are different. file: $0"'
+find . -name vet.sh | xargs -n 1 bash -c \
+    'cmp --silent internal/vet.sh "$0" || echo "files are different. file: $0"'
